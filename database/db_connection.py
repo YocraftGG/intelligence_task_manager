5da65f6856443a49c3e54272ae62eac6b7cf8727
Yohay Halevi <yohaihalevi@gmail.com>
2026-06-17 12:28:25 +0300

    MySQL connection

--- database/db_connection.py
+++ database/db_connection.py
@@ -21,3 +21,43 @@ class DB_connection:
 
         cursor.close()
         conn.close()
+
+
+    
+
+    def create_tables():
+        conn = DB_connection.get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS Intelligence_db.agents(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(50),
+                specialty VARCHAR(50),
+                is_active BOOLEAN DEFAULT TRUE ,
+                completed_missions INT DEFAULT 0,
+                failed_missions INT DEFAULT 0,
+                agent_rank ENUM("Junior", "Senior", "Commander")
+            )
+            """
+        )
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS Intelligence_db.missions(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                title VARCHAR(50),
+                description TEXT,
+                location VARCHAR(250),
+                difficulty INT,
+                importance INT,
+                status VARCHAR(50) DEFAULT "NEW",
+                risk_level VARCHAR(50),
+                assigned_agent_id INT DEFAULT NULL
+            )
+            """
+        )
+        conn.commit()
+
+        cursor.close()
+        conn.close()
